81928f13733c75799550bd27043d3ceff552daa4
Elisha Marshall <billups70@yahoo.com>
2016-07-24 23:09:51 -0400

    Black slide view controller

--- DCandCo/CartViewController.swift
+++ DCandCo/CartViewController.swift
@@ -8,6 +8,34 @@
 
 import UIKit
 
-class CartViewController: UIViewController {
+class CartViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var menuItems = ["Categories", "Cart", "All Products", "My Account", "About Me"]
+    var currentItem = "Categories"
+
+    
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return menuItems.count
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("tableCell", forIndexPath: indexPath) 
+        
+        cell.textLabel?.text = menuItems[indexPath.row]
+        cell.textLabel?.textColor = UIColor.whiteColor()
+        cell.backgroundColor = UIColor.blackColor()
+        
+        return cell
+        
+    }
+    
 }
